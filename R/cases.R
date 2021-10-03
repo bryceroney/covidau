424e236d692b511ae7e19fd7e96ccbdb11d7cb4b
Bryce Roney <bryceroney@users.noreply.github.com>
2021-10-03 20:12:46 +1100

    add pkgdown;

--- R/cases.R
+++ R/cases.R
@@ -2,6 +2,7 @@
 #'
 #' @param states a character vector of states to load data for
 #' @import rvest
+#' @importFrom rlang .data
 #'
 #' @examples
 #' data <- cases(c('ACT', 'NSW'))
@@ -19,7 +20,9 @@ load_cases_state <- function(state) {
     html_element("table.DAILY-CASES") %>%
     html_table() %>%
     janitor::clean_names() %>%
-    dplyr::filter(net != "-" & new != "-") %>%
+    dplyr::filter(.data$net != "-" & .data$new != "-") %>%
     dplyr::mutate(date = lubridate::dmy(date)) %>%
-    dplyr::mutate(across(c(new, cases, var, net), ~ as.numeric(gsub(",", "", .x))))
+    dplyr::mutate(dplyr::across(c(.data$new, .data$cases, .data$var, .data$net),
+                                ~ as.numeric(gsub(",", "", .x)))) %>%
+    dplyr::mutate(state = toupper(state))
 }
--- R/scraper.R
+++ R/scraper.R
@@ -3,7 +3,9 @@
 covidlive <- function(endpoint) {
   ret <- httr2::request("https://covidlive.com.au") %>%
     httr2::req_url_path_append(endpoint) %>%
-    httr2::req_user_agent("covidau package (https://github.com/bryceroney/covidau") %>%
+    httr2::req_user_agent(
+      "covidau package (https://github.com/bryceroney/covidau"
+    ) %>%
     httr2::req_perform() %>%
     httr2::resp_body_html()
 
